62d997142f3c287be0b95858187419f0c6c171ce
meet-liuyongkang <1660671346@qq.com>
2020-05-22 21:03:07 +0800

    测试git

--- spring-luban/src/main/java/com/luban/postProcessor/Test.java
+++ spring-luban/src/main/java/com/luban/postProcessor/Test.java
@@ -5,7 +5,7 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Test {
 
 	public static void main(String[] args) {
-		//AnnotationConfigApplicationContext就是spring环境，也叫spring上下文环境
+		//AnnotationConfigApplicationContext就是spring环境，也叫spring上下文环境，git测试一下嘿嘿
 		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppconfigOnlyBean.class);
 		context.register(UserServiceImpl.class);
 	}
